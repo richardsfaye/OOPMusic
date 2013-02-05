d4402fbb3c8fe8fcbcfbd6c5f7557cc0595cfce3
emmclean <emmclean@uw.edu>
2013-02-05 11:59:37 -0800

    Create MaryLambSteelGuitar.java

---
+++ MaryLambSteelGuitar.java
@@ -0,0 +1,13 @@
+import org.jfugue.*; 
+public class MaryLambSteelGuitar 
+{ 
+    public static void main(String[] args) throws InterruptedException 
+            { 
+        Player player = new Player(); 
+        player.play("I25 E3q D3q C3q D3q E3q E3q E3h D3q D3q D3h E3q G3q G3h E3q D3q C3q D3q E3q E3q E3q E3q D3q D3q E3q D3q C3w"); 
+       // Thread.sleep(10000); //Only needed sometimes
+                System.exit(0); // If using Java 1.4 or lower
+    } 
+} 
+//C4 is about mid range piano
+//octave always starts at C
