34e595944a1e754be479f037db04aa206ea7684b
emmclean <emmclean@uw.edu>
2013-02-10 12:03:01 -0800

    Create DisplayNote.java

---
+++ DisplayNote.java
@@ -0,0 +1,19 @@
+//If finalNote method could not lead to display method but isntead, Display method recieved noteDuration and noteType, that would be awesome.
+
+//I do not know how we will deal with placing on the x axis yet. The display method might need to look at the array, and the note's place in the array to determine that.
+
+public class DisplayNote{
+//Rest Method
+//if noteDuration= rest
+//call LoadRightNoteApp
+
+//Repeat this for Quarter, Half, and Whole Method
+
+//Currently working on figuring out how to manifest an image on another image
+
+
+public static void main (String[] args){
+int noteDuration = Integer.parseInt(args[0]);
+int noteType = Integer.parseInt(args[1]);
+}
+}
